7c82e7dbea462f09c22dfa8d6f3cc079cbb98f41
Kushal Zanzari <146122165+KushalZanzari@users.noreply.github.com>
2026-01-30 16:18:54 +0530

    Add Hit the Lottery [A] from Codeforces

---
+++ 996/A - Hit the Lottery/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Main {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        int n = sc.nextInt();        int[] bills = {100, 20, 10, 5, 1};        int count = 0;         for (int bill : bills) {            count += n / bill;             n = n % bill;              }        System.out.println(count);    }}
