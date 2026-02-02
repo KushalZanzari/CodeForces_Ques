7494cdc38a110f1ea50731f5673b61026631a7ec
Kushal Zanzari <146122165+KushalZanzari@users.noreply.github.com>
2026-02-02 21:43:25 +0530

    Add Vasya the Hipster [A] from Codeforces

---
+++ 581/A - Vasya the Hipster/solution.java
@@ -0,0 +1,1 @@
+import java.util.Scanner; public class Main {    public static void main(String[] args) {        Scanner sc = new Scanner(System.in);        int a = sc.nextInt();        int b = sc.nextInt();                int different = Math.min(a, b);        int remaining = Math.abs(a - b);        int same = remaining / 2;                System.out.println(different + " " + same);    }}
